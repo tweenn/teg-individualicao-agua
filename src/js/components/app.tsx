a7e61c814e62e642194fa6ab6d6f15a7ff0c27d2
Felipe Zuntini Abella <felzun@br.ibm.com>
2025-09-30 00:42:28 -0300

    refactor: 💡 more function segregations

--- src/js/components/app.tsx
+++ src/js/components/app.tsx
@@ -1,4 +1,3 @@
-/** biome-ignore-all lint/nursery/useUniqueElementIds: it's fine */
 import { PDFDocument } from 'pdf-lib';
 import JSZip from 'jszip';
 import Uppy from '@uppy/core';
@@ -10,12 +9,11 @@ import {
 
 import { useEffect, useState } from 'preact/compat';
 
-import copyPdfPage from '../utility/copy-pdf-page.js';
-import compressPdf from '../utility/compress-pdf.js';
+import processJobBatch from '../utility/process-job-batch.js';
 import downloadZip from '../utility/download-zip.js';
 
 function App() {
-	const NUMBER_OF_CONCURRENT_FILES = 10;
+	const NUMBER_OF_CONCURRENT_FILES = parseInt(import.meta?.env?.VITE_NUMBER_OF_CONCURRENT_JOBS || 10);
 	const [uppy] = useState(() => new Uppy());
 	const [pdf, setPdf]: [any, any] = useState(false);
 	const [totalPdfPages, setTotalPdfPages] = useState(0)
@@ -54,10 +52,6 @@ function App() {
 		const numberOfJobs = Math.ceil(numberOfPages / NUMBER_OF_CONCURRENT_FILES);
 
 		for (let jobIndex = 0; jobIndex < numberOfJobs; jobIndex ++) {
-			const jobsRunning = jobIndex + 1 === numberOfJobs
-				? (numberOfPages % NUMBER_OF_CONCURRENT_FILES)
-				: NUMBER_OF_CONCURRENT_FILES;
-
 			const pageFrom = (jobIndex * NUMBER_OF_CONCURRENT_FILES) + 1;
 			let pageTo = (jobIndex + 1) * NUMBER_OF_CONCURRENT_FILES;
 			pageTo = pageTo < numberOfPages
@@ -66,23 +60,19 @@ function App() {
 
 			setCurrentPdfPageBeingProcessed(`${pageFrom}~${pageTo}`);
 
-			const promisses = new Array(jobsRunning)
-				.fill(true, 0, jobsRunning)
-				.map(async (_value, index) => {
-					const pdfIndex = jobIndex * NUMBER_OF_CONCURRENT_FILES + index;
-					const pdfPage = pdfIndex + 1;
+			const promises = processJobBatch(
+				jobIndex,
+				numberOfJobs,
+				numberOfPages,
+				pdfDoc,
+			)
 
-					const pdfBytes = await copyPdfPage(
-						pdfDoc,
-						[pdfIndex]
-					);
-
-					const compressedPdfBytes = await compressPdf(pdfBytes);
-
-					zip.file(`Page ${pdfPage}.pdf`, compressedPdfBytes);
+			await Promise.all(promises)
+				.then((answer: any) => {
+					answer.forEach((file: any) => {
+						zip.file(file.name, file.bin);
+					})
 				});
-
-			await Promise.all(promisses);
 		}
 
 		const zippedUint8Array = await zip.generateAsync({ type: "uint8array" });
@@ -99,7 +89,7 @@ function App() {
 
 				<article>
 					<div style={
-						`display: ${pdf === false ? 'inherit' : 'nonee'}`
+						`display: ${pdf === false ? 'inherit' : 'none'}`
 					}>
 						<Dropzone />
 					</div>
---
+++ src/js/utility/process-job-batch.ts
@@ -0,0 +1,36 @@
+import { PDFDocument } from 'pdf-lib';
+
+import copyPdfPage from './copy-pdf-page.js';
+import compressPdf from './compress-pdf.js';
+
+export default (
+	jobIndex: number,
+	numberOfJobs: number,
+	numberOfPages: number,
+	pdfDoc: PDFDocument
+) => {
+	const NUMBER_OF_CONCURRENT_FILES = parseInt(import.meta?.env?.VITE_NUMBER_OF_CONCURRENT_JOBS || 10);
+
+	const jobsRunning = jobIndex + 1 === numberOfJobs
+		? (numberOfPages % NUMBER_OF_CONCURRENT_FILES)
+		: NUMBER_OF_CONCURRENT_FILES;
+
+	return new Array(jobsRunning)
+		.fill(true, 0, jobsRunning)
+		.map(async (_value, index) => {
+			const pdfIndex = jobIndex * NUMBER_OF_CONCURRENT_FILES + index;
+			const pdfPage = pdfIndex + 1;
+
+			const pdfBytes = await copyPdfPage(
+				pdfDoc,
+				[pdfIndex]
+			);
+
+			const compressedPdfBytes = await compressPdf(pdfBytes);
+
+			return {
+				name: `Page ${pdfPage}.pdf`,
+				bin: compressedPdfBytes
+			}
+		});
+}
